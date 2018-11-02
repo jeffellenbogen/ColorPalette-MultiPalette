af3a837b82ce06b1fa5f898c3dd2d65a7d18d2d0
jeffellenbogen <jeffellenbogen@users.noreply.github.com>
2018-11-02 09:00:51 -0600

    Update ColorPalette-MultiPalette.ino

--- ColorPalette-MultiPalette.ino
+++ ColorPalette-MultiPalette.ino
@@ -1,13 +1,15 @@
 #include <FastLED.h>
+#include <elapsedMillis.h>
+elapsedMillis timer1;
 
 #define LED_PIN     3
-#define NUM_LEDS    28
+#define NUM_LEDS    60
 #define BRIGHTNESS  64
 #define LED_TYPE    WS2812
 #define COLOR_ORDER GRB
 CRGB leds[NUM_LEDS];
 
-#define UPDATES_PER_SECOND 100
+int UPDATES_PER_SECOND;
 
 // This example shows several ways to set up and use 'palettes' of colors
 // with FastLED.
@@ -28,7 +30,7 @@ CRGB leds[NUM_LEDS];
 
 
 
-CRGBPalette16 currentPalette, girlPalette, rainPalette;
+CRGBPalette16 currentPalette, girlPalette, rainPalette, cityPalette, puddlePalette;
 TBlendType    currentBlending;
 
 extern CRGBPalette16 myRedWhiteBluePalette;
@@ -39,42 +41,121 @@ void setup() {
     delay( 3000 ); // power-up safety delay
     FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
     FastLED.setBrightness(  BRIGHTNESS );
-    
-    girlPalette = LavaColors_p;
-    rainPalette = OceanColors_p;
+   
     currentBlending = LINEARBLEND;
 }
 
 
 void loop()
 {
-   // ChangePalettePeriodically();
+ 
     
     static uint8_t startIndex = 0;
     startIndex = startIndex + 1; /* motion speed */
-    
-    FillLEDsFromPaletteColors(startIndex);
+
+  if (timer1 < 10000) {
+    FillLEDsFromPaletteColors_scene1(startIndex);
+  }
+  else if ((timer1 > 10000) && (timer1 < 20000)) {
+    FillLEDsFromPaletteColors_scene2(startIndex); 
+  }
+  else if ((timer1 > 20000) && (timer1 < 40000)) {
+    FillLEDsFromPaletteColors_scene3(startIndex); 
+  }
+  else {
+    timer1 = 0;
+  }
+
+   
     
     FastLED.show();
     FastLED.delay(1000 / UPDATES_PER_SECOND);
 }
 
-void FillLEDsFromPaletteColors( uint8_t colorIndex)
+void FillLEDsFromPaletteColors_scene1( uint8_t colorIndex)
 {
     uint8_t brightness = 255;
-     
-     for( int i = 8; i < 11; i++) {
-        leds[i] = ColorFromPalette( girlPalette, colorIndex, brightness, currentBlending);
+    UPDATES_PER_SECOND = 10;
+    girlPalette = solidpurpleColors_p;
+    rainPalette = OceanColors_p;
+    cityPalette = CityColors_p;
+    puddlePalette = ForestColors_p;
+    
+     for( int i = 1; i < 11; i++) {
+        leds[i] = ColorFromPalette( rainPalette, colorIndex, brightness, currentBlending);
         colorIndex += 3;
     }
     
-    for( int i = 14; i < NUM_LEDS; i++) {
-        leds[i] = ColorFromPalette( rainPalette, colorIndex, brightness, currentBlending);
+    for( int i = 17; i < 21; i++) {
+        leds[i] = ColorFromPalette( girlPalette, colorIndex, brightness, currentBlending);
+        colorIndex += 3;
+    }
+    for( int i = 29; i < 44; i++) {
+        leds[i] = ColorFromPalette( puddlePalette, colorIndex, brightness, currentBlending);
+        colorIndex += 3;
+    }
+    for( int i = 44; i < NUM_LEDS; i++) {
+        leds[i] = ColorFromPalette( cityPalette, colorIndex, brightness, currentBlending);
         colorIndex += 3;
     }
 }
 
+void FillLEDsFromPaletteColors_scene2( uint8_t colorIndex)
+{
+    uint8_t brightness = 10;
+    UPDATES_PER_SECOND = 100;
+    girlPalette = LavaColors_p;
+    rainPalette = PartyColors_p;
+    cityPalette = RainbowStripeColors_p;
+    puddlePalette = RainbowStripeColors_p; 
+    
+     for( int i = 1; i < 11; i++) {
+        leds[i] = ColorFromPalette( rainPalette, colorIndex, brightness, currentBlending);
+        colorIndex += 3;
+    }
+    
+    for( int i = 17; i < 21; i++) {
+        leds[i] = ColorFromPalette( girlPalette, colorIndex, brightness, currentBlending);
+        colorIndex += 3;
+    }
+    for( int i = 29; i < 44; i++) {
+        leds[i] = ColorFromPalette( puddlePalette, colorIndex, brightness, currentBlending);
+        colorIndex += 3;
+    }
+    for( int i = 44; i < NUM_LEDS; i++) {
+        leds[i] = ColorFromPalette( cityPalette, colorIndex, brightness, currentBlending);
+        colorIndex += 3;
+    }
+}
 
+void FillLEDsFromPaletteColors_scene3( uint8_t colorIndex)
+{
+    uint8_t brightness = 100;
+    uint8_t brightness_girl = 255;
+    UPDATES_PER_SECOND = 25;
+    girlPalette = ForestColors_p;
+    rainPalette = CloudColors_p;
+    cityPalette = OceanColors_p;
+    puddlePalette = solidpurpleColors_p; 
+    
+     for( int i = 1; i < 11; i++) {
+        leds[i] = ColorFromPalette( rainPalette, colorIndex, brightness, currentBlending);
+        colorIndex += 3;
+    }
+    
+    for( int i = 17; i < 21; i++) {
+        leds[i] = ColorFromPalette( girlPalette, colorIndex, brightness_girl, currentBlending);
+        colorIndex += 3;
+    }
+    for( int i = 29; i < 44; i++) {
+        leds[i] = ColorFromPalette( puddlePalette, colorIndex, brightness, currentBlending);
+        colorIndex += 3;
+    }
+    for( int i = 44; i < NUM_LEDS; i++) {
+        leds[i] = ColorFromPalette( cityPalette, colorIndex, brightness, currentBlending);
+        colorIndex += 3;
+    }
+}
 // There are several different palettes of colors demonstrated here.
 //
 // FastLED provides several 'preset' palettes: RainbowColors_p, RainbowStripeColors_p,
@@ -83,100 +164,3 @@ void FillLEDsFromPaletteColors( uint8_t colorIndex)
 // Additionally, you can manually define your own color palettes, or you can write
 // code that creates color palettes on the fly.  All are shown here.
 
-void ChangePalettePeriodically()
-{
-
-        currentPalette = PartyColors_p;           
-        currentBlending = LINEARBLEND; 
-    
-    
-}
-
-// This function fills the palette with totally random colors.
-void SetupTotallyRandomPalette()
-{
-    for( int i = 0; i < 16; i++) {
-        currentPalette[i] = CHSV( random8(), 255, random8());
-    }
-}
-
-// This function sets up a palette of black and white stripes,
-// using code.  Since the palette is effectively an array of
-// sixteen CRGB colors, the various fill_* functions can be used
-// to set them up.
-void SetupBlackAndWhiteStripedPalette()
-{
-    // 'black out' all 16 palette entries...
-    fill_solid( currentPalette, 16, CRGB::Black);
-    // and set every fourth one to white.
-    currentPalette[0] = CRGB::White;
-    currentPalette[4] = CRGB::White;
-    currentPalette[8] = CRGB::White;
-    currentPalette[12] = CRGB::White;
-    
-}
-
-// This function sets up a palette of purple and green stripes.
-void SetupPurpleAndGreenPalette()
-{
-    CRGB purple = CHSV( HUE_PURPLE, 255, 255);
-    CRGB green  = CHSV( HUE_GREEN, 255, 255);
-    CRGB black  = CRGB::Black;
-    
-    currentPalette = CRGBPalette16(
-                                   green,  green,  black,  black,
-                                   purple, purple, black,  black,
-                                   green,  green,  black,  black,
-                                   purple, purple, black,  black );
-}
-
-
-// This example shows how to set up a static color palette
-// which is stored in PROGMEM (flash), which is almost always more
-// plentiful than RAM.  A static PROGMEM palette like this
-// takes up 64 bytes of flash.
-const TProgmemPalette16 myRedWhiteBluePalette_p PROGMEM =
-{
-    CRGB::Red,
-    CRGB::Gray, // 'white' is too bright compared to red and blue
-    CRGB::Blue,
-    CRGB::Black,
-    
-    CRGB::Red,
-    CRGB::Gray,
-    CRGB::Blue,
-    CRGB::Black,
-    
-    CRGB::Red,
-    CRGB::Red,
-    CRGB::Gray,
-    CRGB::Gray,
-    CRGB::Blue,
-    CRGB::Blue,
-    CRGB::Black,
-    CRGB::Black
-};
-
-
-
-// Additionl notes on FastLED compact palettes:
-//
-// Normally, in computer graphics, the palette (or "color lookup table")
-// has 256 entries, each containing a specific 24-bit RGB color.  You can then
-// index into the color palette using a simple 8-bit (one byte) value.
-// A 256-entry color palette takes up 768 bytes of RAM, which on Arduino
-// is quite possibly "too many" bytes.
-//
-// FastLED does offer traditional 256-element palettes, for setups that
-// can afford the 768-byte cost in RAM.
-//
-// However, FastLED also offers a compact alternative.  FastLED offers
-// palettes that store 16 distinct entries, but can be accessed AS IF
-// they actually have 256 entries; this is accomplished by interpolating
-// between the 16 explicit entries to create fifteen intermediate palette
-// entries between each pair.
-//
-// So for example, if you set the first two explicit entries of a compact 
-// palette to Green (0,255,0) and Blue (0,0,255), and then retrieved 
-// the first sixteen entries from the virtual palette (of 256), you'd get
-// Green, followed by a smooth gradient from green-to-blue, and then Blue.
